2008bb488adb6e0ce487db0b9b24c51f36d44698
Maximiliano Garbino <maximilianogarbino981@gmail.com>
2024-08-10 20:55:20 -0300

    hosdaa

--- main.py
+++ main.py
@@ -9,5 +9,5 @@ async def predict_image(image: UploadFile = File(...)):
     return {"result": "Melanoma"}
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
